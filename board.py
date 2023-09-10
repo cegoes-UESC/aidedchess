45695296cb6557b38b2a8cfa827991267102598b
Almir Neto <almirneto338@gmail.com>
2023-09-10 18:59:16 -0300

    Adding semi board localisation

--- board.py
+++ board.py
@@ -1,5 +1,6 @@
 import cv2 as cv
 import numpy as np
+import pandas as pd
 
 from matplotlib import pyplot as plt
 
@@ -12,8 +13,8 @@ images = [cv.imread(str(path / image)) for image in images]
 
 
 class ImageCanny:
-    #low, high = 360, 360
-    low, high = 0, 600
+    # low, high = 360, 360
+    low, high = 0, 500
 
     def setLow(self, l):
         self.low = l
@@ -55,7 +56,7 @@ cv.createTrackbar("th", "tracks", H.th, 500, H.setTh)
 imgs = []
 
 for image in images:
-    i = cv.GaussianBlur(image, (3, 3), 0)
+    i = cv.GaussianBlur(image, (9, 9), 0)
     resized = cv.resize(i, (500, 500))
     gray = cv.cvtColor(resized, cv.COLOR_BGR2GRAY)
     imgs.append(
@@ -68,10 +69,10 @@ for image in images:
 
 
 def getColor(degree):
-    if degree <= 45:
+    if degree <= 90:
         return (255, 0, 0)
 
-    if degree <= 90:
+    if degree <= 180:
         return (0, 255, 0)
 
     return (0, 0, 255)
@@ -81,11 +82,21 @@ def getDegree(theta):
     return theta * (180 / np.pi)
 
 
+def splitLinesGroups(dataframe):
+    dif = dataframe.diff()
+    idx = dif.theta.idxmax()
+    middleTheta = dataframe[dataframe.index == idx].iloc[0].theta
+    return (
+        dataframe[dataframe.theta < middleTheta],
+        dataframe[dataframe.theta >= middleTheta],
+    )
+
+
 hough = []
 once = 1
 
 while once == 1:
-    # once = 0
+    once = 0
     for index, images in enumerate(imgs):
         original = images.get("resize").copy()
 
@@ -94,11 +105,17 @@ while once == 1:
         edges = cv.Canny(gray_resized, C.low, C.high)
         lines = cv.HoughLines(edges, H.rho, H.theta, H.th)
 
+        # radon = cv.ximgproc.RadonTransform(
+        #     edges, theta=1, start_angle=0, end_angle=360, norm=True
+        # )
+
+        dataset = []
+
         if lines is not None:
             r = []
             t = []
-            xx = []
-            yy = []
+            x = []
+            y = []
             for line in lines:
                 rho, theta = line[0]
                 r.append(rho)
@@ -108,18 +125,54 @@ while once == 1:
                 x0 = a * rho
                 y0 = b * rho
 
-                xx.append(x0)
-                yy.append(y0)
+                x.append(x0)
+                y.append(y0)
 
-                x1 = int(x0 + 1000 * (-b))
-                y1 = int(y0 + 1000 * (a))
-                x2 = int(x0 - 1000 * (-b))
-                y2 = int(y0 - 1000 * (a))
+                # x1 = int(x0 + 1000 * (-b))
+                # y1 = int(y0 + 1000 * (a))
+                # x2 = int(x0 - 1000 * (-b))
+                # y2 = int(y0 - 1000 * (a))
 
-                cv.circle(original, (int(x0), int(y0)), 1, (255, 0, 255), 2)
-                cv.line(original, (x1, y1), (x2, y2), getColor(getDegree(theta)), 1)
+                # cv.circle(original, (int(x0), int(y0)), 1, (255, 0, 255), 2)
 
-            hough.append({"rho": r, "theta": t, "x": xx, "y": yy})
+                # if index == 0:
+                #     if theta >= 2.949606:
+                #         c = (0, 0, 255)
+                #     else:
+                #         c = (255, 0, 0)
+
+                # if index == 1:
+                #     if theta >= 2.513274:
+                #         c = (0, 0, 255)
+                #     else:
+                #         c = (255, 0, 0)
+                # cv.line(original, (x1, y1), (x2, y2), c, 1)
+
+            hough.append({"rho": r, "theta": t, "x": x, "y": y, "radon": []})
+            dt = pd.DataFrame({"rho": r, "theta": t, "x": x, "y": y})
+            dt = dt.sort_values(by=["theta", "rho"])
+            lines1, lines2 = splitLinesGroups(dt)
+            for row in lines1.itertuples():
+                a = np.cos(row.theta)
+                b = np.sin(row.theta)
+                x1 = int(row.x + 1000 * (-b))
+                y1 = int(row.y + 1000 * (a))
+                x2 = int(row.x - 1000 * (-b))
+                y2 = int(row.y - 1000 * (a))
+
+                cv.line(original, (x1, y1), (x2, y2), (0, 0, 255), 1)
+
+                # cv.circle(original, (int(row.x), int(row.y)), 1, (0, 0, 255), 2)
+
+            for row in lines2.itertuples():
+                a = np.cos(row.theta)
+                b = np.sin(row.theta)
+                x1 = int(row.x + 1000 * (-b))
+                y1 = int(row.y + 1000 * (a))
+                x2 = int(row.x - 1000 * (-b))
+                y2 = int(row.y - 1000 * (a))
+
+                cv.line(original, (x1, y1), (x2, y2), (255, 0, 255), 1)
 
         cv.imshow("chess_" + str(index), edges)
         cv.imshow("chessboard_" + str(index), original)
@@ -128,16 +181,14 @@ while once == 1:
         break
 
 
-domain = np.arange(-np.pi, np.pi, 0.05)
+domain = np.arange(-np.pi * 4, np.pi * 4, 0.01)
 
-for h in hough:
+for index, h in enumerate(hough):
     theta = h.get("theta")
     rho = h.get("rho")
 
-    plt.figure()
     x = h.get("x")
     y = h.get("y")
-    plt.scatter(x, y, s=rho)
 
     plt.figure()
     plt.axes().set_facecolor("black")
@@ -149,7 +200,9 @@ for h in hough:
     dd = [getDegree(t) for t in theta]
     plt.scatter(theta, rho, color=(1, 0, 0, 0.5))
 
+    # radon = h.get("radon")
+    # cv.imshow("radon_" + str(index), radon)
 
-plt.show()
 
+plt.show(block=False)
 cv.waitKey(0)
