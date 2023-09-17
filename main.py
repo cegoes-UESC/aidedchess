748544141c9711e41d70a4063d2ea285ba58928b
Almir Neto <almirneto338@gmail.com>
2023-09-17 12:56:36 -0300

    - Reading from webcam processing

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from Board import Board
 import cv2 as cv
 
 from pathlib import Path
+from time import time
 
 b = Board(False)
 
@@ -12,6 +13,26 @@ images = p.glob("*")
 
 images = [cv.imread(str(i)) for i in list(images)]
 
-for image in images:
-    b.setImage(image)
-    b.process()
+v = cv.VideoCapture(0)
+
+while v.isOpened():
+    _, frame = v.read()
+    cv.imshow("me", frame)
+    b.setImage(frame)
+    image = frame
+    try:
+        image = b.process()
+    except:
+        pass
+    cv.imshow("result", image)
+    if cv.waitKey(1) == ord("q"):
+        break
+
+
+v.release()
+cv.waitKey(0)
+cv.destroyAllWindows()
+
+# for image in images:
+#     b.setImage(image)
+#     b.process()
