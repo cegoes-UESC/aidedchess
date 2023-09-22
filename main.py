a75bdc2b4ecd300057ef8c789521f38db5bf5b88
Almir Neto <almirneto338@gmail.com>
2023-09-21 22:52:10 -0300

    Improving file reading

--- main.py
+++ main.py
@@ -5,34 +5,32 @@ import cv2 as cv
 from pathlib import Path
 from time import time
 
-b = Board(False)
+b = Board(True)
 
 p = Path("resources")
-
 images = p.glob("*")
 
-images = [cv.imread(str(i)) for i in list(images)]
+# v = cv.VideoCapture(0)
 
-v = cv.VideoCapture(0)
+# while v.isOpened():
+#     _, frame = v.read()
+#     cv.imshow("me", frame)
+#     b.setImage(frame)
+#     image = frame
+#     try:
+#         image = b.process()
+#     except:
+#         pass
+#     cv.imshow("result", image)
+#     if cv.waitKey(1) == ord("q"):
+#         break
 
-while v.isOpened():
-    _, frame = v.read()
-    cv.imshow("me", frame)
-    b.setImage(frame)
-    image = frame
-    try:
-        image = b.process()
-    except:
-        pass
-    cv.imshow("result", image)
-    if cv.waitKey(1) == ord("q"):
-        break
+# v.release()
 
+for image in list(images):
+    im = cv.imread(str(image))
+    b.setImage(im)
+    result = b.process()
 
-v.release()
 cv.waitKey(0)
 cv.destroyAllWindows()
-
-# for image in images:
-#     b.setImage(image)
-#     b.process()
