e5d39f43e0d36348819bd8ea6cb459e041c6c6d7
Almir Neto <almirneto338@gmail.com>
2023-12-14 17:32:50 -0300

    Adding the AA code [TODO]

--- aa.py
+++ aa.py
@@ -1,31 +1,37 @@
-from ultralytics import YOLO
-from cvat_sdk import models
-
-import cvat_sdk.auto_annotation as cvataa
-from cvat_sdk import make_client
 import PIL
+from typing import List
+from cvat_sdk import models
+from ultralytics import YOLO
+from cvat_sdk import make_client
+import cvat_sdk.auto_annotation as cvataa
 
 
 class AnnotShapes:
     def __init__(self) -> None:
-        self.model = YOLO("best-pose-07-12.pt")
+        self.model = YOLO("models/best-pose-07-12.pt")
+        self.model.eval()
 
     @property
-    def spec() -> cvataa.DetectionFunctionSpec:
-        return cvataa.DetectionFunctionSpec(labels=[cvataa.label_spec("board", 1)])
+    def spec(self) -> cvataa.DetectionFunctionSpec:
+        return cvataa.DetectionFunctionSpec(
+            labels=[
+                # cvataa.label_spec(name, id) for (name, id) in self.model.names.items(),
+                cvataa.skeleton_label_spec(name, id)
+                for (name, id) in self.model.names.items()
+            ],
+        )
 
-    def detect(self, context, image: PIL.Image.Image):
+    def detect(
+        self, context, image: PIL.Image.Image
+    ) -> List[models.LabeledShapeRequest]:
         results = self.model(image)
 
         return [
-            cvataa.skeleton(label.item(), [x.item() for x in skeleton])
+            cvataa.skeleton(label.item(), skeleton.xy.tolist())
             for result in results
-            for skeleton, label in zip(result["keypoints"], result["labels"])
+            for skeleton, label in zip(result.keypoints, result.boxes.cls)
         ]
 
 
-
-
 with make_client(host="localhost", credentials=("user", "password")) as client:
-    # annotate task 12345 using Faster R-CNN
     cvataa.annotate_task(client, 41617, AnnotShapes)
