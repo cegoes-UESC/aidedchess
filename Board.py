39fd67bf37a3071fc4ee06cf80d9cd6e30a2adb3
Almir Neto <almirneto338@gmail.com>
2024-01-17 00:02:11 -0300

    Undoing the perpective of intersected points

--- Board.py
+++ Board.py
@@ -12,27 +12,56 @@ class Board:
         self.points = pts
 
     def _try(self):
-        self.outPoints = np.float32([[0, 0], [300, 0], [300, 300], [0, 300]])
+        self.outPoints = np.float32([[0, 0], [500, 0], [500, 500], [0, 500]])
 
         M = cv.getPerspectiveTransform(self.points, self.outPoints)
+        self.trans = M
 
-        return cv.warpPerspective(self.image, M, (300, 300))
+        return cv.warpPerspective(self.image, M, (500, 500))
+
+    def un(self, inn):
+        w, h = self.image.shape[1], self.image.shape[0]
+        return cv.warpPerspective(inn, self.trans, (w, h), flags=cv.WARP_INVERSE_MAP)
 
 
 root = Path("datasets/chess/images/train")
 
-src = "datasets/chess/images/train/IMG_2661.jpg"
+image = Path("datasets/chess/images/train/IMG_2651.jpg")
 
 images = list(root.glob("*.jpg"))
 
-for image in images:
-    pred = model.predict(image.resolve(), verbose=False, classes=[0], save=False)
+# for image in images:
+pred = model.predict(image.resolve(), verbose=False, classes=[0], save=False)
 
-    pts = pred[0].keypoints.xy.tolist()[0]
-    pts = np.float32(pts)
+pts = pred[0].keypoints.xy.tolist()[0]
+pts = np.float32(pts)
 
-    b = Board(cv.imread(str(image.resolve())), pts)
+b = Board(cv.imread(str(image.resolve())), pts)
 
-    per = b._try()
+per = b._try()
 
-    cv.imwrite("try/" + image.name, per)
+from _Board import Board as B
+
+bbb = B(debug=True)
+
+bbb.setImage(per)
+
+res = bbb.process()
+
+cv.namedWindow("resized", cv.WINDOW_GUI_EXPANDED)
+cv.imshow("resized", res)
+
+cv.imshow("try", per)
+
+oooooo = b.un(res)
+
+cv.namedWindow("ooo", cv.WINDOW_GUI_EXPANDED)
+cv.imshow("ooo", oooooo)
+
+
+im = cv.imread(str(image.resolve()))
+
+cv.namedWindow("r", cv.WINDOW_GUI_EXPANDED)
+cv.imshow("r", im)
+
+cv.waitKey(0)
