d39a249bc6d994766c2ea3ac67734dc920534c33
Almir Neto <almirneto338@gmail.com>
2024-01-18 22:00:15 -0300

    Moving perspective logic to a class

---
+++ Perspective.py
@@ -0,0 +1,25 @@
+import cv2 as cv
+import numpy as np
+
+
+class Perspective:
+    def __init__(self, image, pts=(), size=500):
+        self.image = image
+        self.points = pts
+        self.matrix = []
+        self.size = size
+
+    def applyPerspective(self):
+        self.outPoints = np.float32(
+            [[0, 0], [self.size, 0], [self.size, self.size], [0, self.size]]
+        )
+
+        self.matrix = cv.getPerspectiveTransform(self.points, self.outPoints)
+
+        return cv.warpPerspective(self.image, self.matrix, (self.size, self.size))
+
+    def ll(self, points):
+        # w, h = self.image.shape[1], self.image.shape[0]
+        inverseMatrix = np.linalg.inv(self.matrix)
+        return cv.perspectiveTransform(points, inverseMatrix)
+        # return cv.warpPerspective(inn, self.trans, (w, h), flags=cv.WARP_INVERSE_MAP)
