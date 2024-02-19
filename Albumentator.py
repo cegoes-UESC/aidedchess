a9faf1a3e5123f682c7c4032ed18ca14333e8816
Almir Neto <almirneto338@gmail.com>
2024-02-19 20:51:03 -0300

    Fixing albumentations

--- Albumentator.py
+++ Albumentator.py
@@ -2,14 +2,6 @@ import cv2 as cv
 from pathlib import Path
 import albumentations as A
 
-transform = A.Compose(
-    [
-        A.RandomCrop(width=1024, height=1024),
-        A.RandomBrightnessContrast(p=0.2),
-    ],
-    bbox_params=A.BboxParams(format="yolo"),
-    keypoint_params=A.KeypointParams(format="xy", remove_invisible=False),
-)
 
 path = Path("datasets/split")
 
@@ -34,7 +26,15 @@ for im in images:
 
     cv.namedWindow("image", cv.WINDOW_GUI_NORMAL)
     bs, ks = [], []
-    for l in label_file:
+    targets = {}
+    arguments = {}
+    size = 0
+    for item_idx, l in enumerate(label_file):
+        size = size + 1
+        if item_idx != 0:
+            targets["bboxes" + str(item_idx - 1)] = "bboxes"
+            targets["keypoints" + str(item_idx - 1)] = "keypoints"
+
         labels_content = l.split(" ")
         labels_content = labels_content[:-1]
         _class = int(labels_content[0])
@@ -55,16 +55,42 @@ for im in images:
         for idx, (kw, kh) in enumerate(kpts):
             kpts[idx] = [int(kw * w), int(kh * h)]
 
-        ks.extend(kpts)
+        if item_idx != 0:
+            arguments["keypoints" + str(item_idx - 1)] = kpts
+            arguments["bboxes" + str(item_idx - 1)] = bs
+        else:
+            arguments["keypoints"] = kpts
+            arguments["bboxes"] = bs
+    print(arguments)
 
-    trans = transform(image=image, bboxes=bs, keypoints=ks)
+    transform = A.Compose(
+        [
+            # A.RandomCrop(width=1024, height=1024),
+            A.Normalize(),
+            A.Perspective(),
+            A.ColorJitter(),
+            A.MotionBlur(),
+            A.ChannelShuffle(),
+            # A.GlassBlur(),
+            A.GaussianBlur(),
+            A.Rotate(border_mode=cv.BORDER_TRANSPARENT),
+            A.RandomBrightnessContrast(p=0.2),
+        ],
+        bbox_params=A.BboxParams(format="yolo"),
+        keypoint_params=A.KeypointParams(format="xy", remove_invisible=False),
+        additional_targets=targets,
+    )
+
+    trans = transform(image=image, **arguments)
     trans_image = trans["image"]
 
-    for kk in trans["keypoints"]:
-        cv.circle(trans_image, kk, 7, (0, 255, 0), 50, 10)
+    for i in range(size):
+        key = "keypoints" + (str(i - 1) if i != 0 else "")
+        for kk in trans[key]:
+            kk = list(map(int, kk))
+            cv.circle(trans_image, kk, 7, (0, 255, 0), 20, 10)
     cv.imshow("image", trans_image)
     cv.waitKey(0)
-    print(labels)
 
 
 # READ dataset root path (from the base, before images and labels)
