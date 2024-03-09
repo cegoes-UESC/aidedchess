ad1d9279daf24bca63d5f634bab4cec748e7f61e
Almir Neto <almirneto338@gmail.com>
2024-03-09 17:28:13 -0300

    Adding a script to move validation files

---
+++ move_val.py
@@ -0,0 +1,22 @@
+import json, os
+from sys import argv
+
+p = argv[1]
+
+
+file = open("annotations/" + p + "/keypoints.json", "r")
+
+data = json.load(file)
+
+ids = data["val"]
+
+for name in ids:
+    os.rename(
+        "datasets/albumented/images/train/" + name,
+        "datasets/albumented/images/val/" + name,
+    )
+    n = name.split(".")[0]
+    os.rename(
+        "datasets/albumented/labels/train/" + n + ".txt",
+        "datasets/albumented/labels/val/" + n + ".txt",
+    )
