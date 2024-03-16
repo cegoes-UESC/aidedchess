2b14b833a68e3d0c6cc47fcf2901c19643e4b508
Almir Neto <almirneto338@gmail.com>
2024-03-16 15:36:16 -0300

    Fixing categories exported by CVAT

--- AnnotationConverter.py
+++ AnnotationConverter.py
@@ -1,7 +1,24 @@
 from ultralytics.data.converter import convert_coco
 from sys import argv
+from pathlib import Path
+import json
 
 PATH = argv[1]
+
+path = Path("annotations/" + PATH)
+files = list(path.glob("*.json"))
+
+for f in files:
+    file = open(f, "r")
+    data = json.load(file)
+    file.close()
+    for idx, c in enumerate(data["categories"]):
+        data["categories"][idx]["id"] = int(c["id"] / 5 + 1)
+    for idx, a in enumerate(data["annotations"]):
+        data["annotations"][idx]["category_id"] = int(a["category_id"] / 5 + 1)
+    file = open(f, "w")
+    json.dump(data, file)
+    file.close()
 
 
 def convert():
