383c6a37d6d3b609ea637cbf04d8344e448b8e50
Almir Neto <almirneto338@gmail.com>
2024-03-30 17:25:40 -0300

    Improving annotation corrections before conversion

--- AnnotationConverter.py
+++ AnnotationConverter.py
@@ -13,6 +13,17 @@ for f in files:
         data["categories"][idx]["id"] = int(c["id"] / 5 + 1)
     for idx, a in enumerate(data["annotations"]):
         data["annotations"][idx]["category_id"] = int(a["category_id"] / 5 + 1)
+        bbox = data["annotations"][idx]["bbox"]
+        if bbox[2] == 0:
+            data["annotations"][idx]["bbox"][2] = 50
+        if bbox[3] == 0:
+            data["annotations"][idx]["bbox"][3] = 50
+        keypoints = data["annotations"][idx]["keypoints"]
+        v1, v2, v3, v4 = keypoints[2], keypoints[5], keypoints[8], keypoints[11]
+        for idx, v in enumerate([v1, v2, v3, v4]):
+            if v == 0:
+                data["annotations"][idx]["keypoints"][idx - 1] = 0
+                data["annotations"][idx]["keypoints"][idx - 2] = 0
     file = open(f, "w")
     json.dump(data, file)
     file.close()
