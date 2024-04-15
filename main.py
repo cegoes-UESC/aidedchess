f6cc6b1c7e5a6d732a70204408beb1ca9acd9acf
Almir Neto <almirneto338@gmail.com>
2024-04-14 23:38:32 -0300

    Testing logic to check if piece is withing a grid cell

--- main.py
+++ main.py
@@ -4,14 +4,11 @@ from Board import Board
 from pathlib import Path
 from ultralytics import YOLO
 from Perspective import Perspective
+from ChessPiece import ChessPiece
 
 model = YOLO("models/pose.pt")
-
-
 image = Path("images/IMG_2658.jpg")
-
 prediction = model.predict(image.resolve(), verbose=False, save=False)[0]
-
 
 classes, keypoints = prediction.boxes.cls, prediction.keypoints.xy
 
@@ -19,9 +16,9 @@ for c, k in zip(classes, keypoints):
     if c.item() == 0:
         boardKeypoints = np.float32(k.tolist())
         continue
-    print(c.item(), k)
 
 im = cv.imread(str(image.resolve()))
+squares_overlay = im.copy()
 
 perspective = Perspective(im, boardKeypoints)
 boardPerspective = perspective.apply()
@@ -29,21 +26,76 @@ boardPerspective = perspective.apply()
 board = Board(debug=True)
 board.setImage(boardPerspective)
 
-boardResized, centers, (horizontal, vertical) = board.process()
-boardIntersections = perspective.undoPerspective(np.array([centers]))
+boardResized, centers, (horizontal, vertical), squares = board.process()
+boardSquares = perspective.undoPerspective(np.array(squares))
 
-intersections = boardIntersections[0]
+centroids, chessboard = [], []
 
-for point in intersections:
-    cv.drawMarker(
-        im,
-        (int(point[0]), int(point[1])),
-        (255, 0, 255),
-        cv.MARKER_CROSS,
-        50,
-        3,
+pieces = []
+
+for c, k in zip(classes, keypoints):
+
+    piece = ChessPiece()
+    piece.setClass(int(c.item()))
+
+    if c.item() == 0:
+        continue
+    for idx, i in enumerate(k):
+        piece.addPoint(i)
+    centroid = piece.getCentroid()
+    centroids.append(centroid)
+
+for sq in boardSquares:
+    item = []
+    item.append(sq)
+    for c in centroids:
+        p = np.array(sq, dtype=np.float32)
+        if cv.pointPolygonTest(p, c, False) > 0:
+            item.append(c)
+    if len(item) == 1:
+        item.append(0)
+    chessboard.append(item)
+
+
+for sq, c in chessboard:
+    if c == 0:
+        cv.fillConvexPoly(
+            squares_overlay,
+            np.array(
+                [
+                    [
+                        [sq[0][0], sq[0][1]],
+                        [sq[1][0], sq[1][1]],
+                        [sq[2][0], sq[2][1]],
+                        [sq[3][0], sq[3][1]],
+                    ]
+                ],
+                dtype=np.int32,
+            ),
+            (0, 0, 255),
+        )
+        continue
+
+    cv.drawMarker(im, (int(c[0]), int(c[1])), (255, 0, 255), cv.MARKER_DIAMOND, 15, 15)
+
+    cv.fillConvexPoly(
+        squares_overlay,
+        np.array(
+            [
+                [
+                    [sq[0][0], sq[0][1]],
+                    [sq[1][0], sq[1][1]],
+                    [sq[2][0], sq[2][1]],
+                    [sq[3][0], sq[3][1]],
+                ]
+            ],
+            dtype=np.int32,
+        ),
+        (255, 255, 0),
     )
 
+
+im = cv.addWeighted(squares_overlay, 0.4, im, 1 - 0.4, 0)
 
 cv.namedWindow("markers", cv.WINDOW_GUI_EXPANDED)
 while True:
