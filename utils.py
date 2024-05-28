a930f3420d9a345d8bea11a08bffa54a14083ae3
Almir Neto <almirneto338@gmail.com>
2024-05-28 10:03:15 -0300

    Moving helper functions to specific file

---
+++ utils.py
@@ -0,0 +1,39 @@
+from pathlib import Path
+
+
+def getLabel(image):
+    name = image.name.split(".")[0]
+    label_name = name + ".txt"
+
+    labels_dir = Path("datasets/chess/labels/train")
+    label = labels_dir / label_name
+    return label.resolve()
+
+
+def getBoardKeypoints(image):
+    label = getLabel(image)
+
+    label_f = open(label, "r")
+
+    kpts = []
+
+    for l in label_f:
+        _ = l.split(" ")
+        cls = _[0]
+        if cls == "0":
+            kpts = _[5:]
+            break
+    return kpts
+
+
+def convertToPx(image, kpts):
+    ks = []
+    for idx, k in enumerate(kpts):
+        if idx in [2, 5, 8, 11]:
+            continue
+        if idx in [0, 3, 6, 9]:
+            k = float(k) * image.shape[1]
+        else:
+            k = float(k) * image.shape[0]
+        ks.append(k)
+    return ks
