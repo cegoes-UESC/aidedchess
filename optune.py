130a65ffd1ec9c7387fc2df879966dd298933ebe
Almir Neto <almirneto338@gmail.com>
2024-05-28 10:03:36 -0300

    Adding optuna optimization code

--- optune.py
+++ optune.py
@@ -1,40 +1,59 @@
 import optuna
 from ultralytics import YOLO
-from ultralytics.utils.metrics import PoseMetrics
 
 
 def objective(trial):
-    args = {
-        "lr0": trial.suggest_float("lr0", 0.001, 0.5),
-        "momentum": trial.suggest_float("momentum", 0.1, 0.9),
-        "weight_decay": trial.suggest_float("weight_decay", 0.0001, 0.001),
-        "warmup_epochs": trial.suggest_float("warmup_epochs", 1, 5),
-        "warmup_momentum": trial.suggest_float("warmup_momentum", 0.01, 0.99),
-        "warmup_bias_lr": trial.suggest_float("warmup_bias_lr", 0.1, 0.9),
-        "box": trial.suggest_float("box", 1.0, 9.0),
-        "cls": trial.suggest_float("cls", 0.5, 10.0),
-        "pose": trial.suggest_float("pose", 6.0, 24.0),
-        "kobj": trial.suggest_float("kobj", 1.0, 10.0),
-    }
+    if trial.number == 0:
+        args = {"epochs": 100, "batch": 16}
+    else:
+        args = {
+            "optimizer": "AdamW",
+            "lr0": trial.suggest_float("lr0", 0.001, 0.5),
+            "momentum": trial.suggest_float("momentum", 0.9, 0.999),
+            "weight_decay": trial.suggest_float("weight_decay", 0.0001, 0.001),
+            "warmup_epochs": trial.suggest_int("warmup_epochs", 3, 10),
+            "warmup_momentum": trial.suggest_float("warmup_momentum", 0.01, 0.99),
+            "warmup_bias_lr": trial.suggest_float("warmup_bias_lr", 0.1, 0.9),
+            "box": trial.suggest_float("box", 0.5, 2.0),
+            "cls": trial.suggest_float("cls", 6.0, 12.0),
+            "pose": trial.suggest_float("pose", 12.0, 24.0),
+            "kobj": trial.suggest_float("kobj", 2.0, 12.0),
+            "batch": trial.suggest_int("batch", 8, 16),
+            "epochs": trial.suggest_int("epochs", 25, 100),
+            "dropout": trial.suggest_float("dropout", 0.05, 0.2),
+        }
 
-    model: YOLO = YOLO("pose_30_01_2024.pt")
+    model: YOLO = YOLO("models/pose_final.pt")
 
-    model.train(data="chess.yaml", epochs=100, imgsz=640, **args)
-    validation: PoseMetrics = model.val(data="chess.yaml")
+    model.train(
+        data="chess.yaml",
+        imgsz=640,
+        val=False,
+        fliplr=0,
+        flipud=0,
+        scale=0,
+        translate=0,
+        hsv_v=0,
+        hsv_s=0,
+        hsv_h=0,
+        erasing=0,
+        augment=False,
+        **args
+    )
+    metrics = model.val(data="chess.yaml")
 
-    return validation.fitness, validation.pose.mp, validation.pose.mr
+    return metrics.fitness
 
 
-study = optuna.create_study(directions=["maximize", "maximize", "maximize"])
+storage = optuna.storages.JournalStorage(
+    optuna.storages.JournalFileStorage("./optuna.log"),
+)
 
-study.optimize(objective, n_trials=10)
+study = optuna.create_study(
+    direction="maximize",
+    study_name="chess-optune",
+    storage=storage,
+    load_if_exists=True,
+)
 
-
-print(f"Study completed for: {len(study.trials)}")
-
-
-trial_with_highest_accuracy = max(study.best_trials, key=lambda t: t.values[1])
-print(f"Trial with highest accuracy: ")
-print(f"\tnumber: {trial_with_highest_accuracy.number}")
-print(f"\tparams: {trial_with_highest_accuracy.params}")
-print(f"\tvalues: {trial_with_highest_accuracy.values}")
+study.optimize(objective, n_trials=1)
