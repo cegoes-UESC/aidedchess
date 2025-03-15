5a943809363d391ff03950165d44ce286965756e
Almir Neto <almirneto338@gmail.com>
2025-03-15 18:17:38 -0300

    refactor: importing function

--- aided_chess/albumentation/__init__.py
+++ aided_chess/albumentation/__init__.py
@@ -1,2 +1,4 @@
+# ruff: noqa: F401
 from .albument import augment as augment
-from .yolo_albumentator import process as process
+from .yolo_albumentator import process as yolo_augment
+from .coco_albumentator import process as coco_process
